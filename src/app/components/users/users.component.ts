99364004137906072f1f88a747194ab87fdbd21a
Nazarii Ryhus <superkriper45@gmail.com>
2023-12-06 15:11:12 +0200

    lesson 1

---
+++ src/app/components/users/users.component.ts
@@ -0,0 +1,35 @@
+import {Component, OnInit} from '@angular/core';
+import {NgForOf, NgIf} from "@angular/common";
+
+import {UserService} from "../../services";
+import {IUser} from "../../interfaces";
+import {UserComponent} from "../user/user.component";
+import {UserDetailsComponent} from "../user-details/user-details.component";
+
+@Component({
+  selector: 'app-users',
+  standalone: true,
+  imports: [
+    UserComponent,
+    NgForOf,
+    UserDetailsComponent,
+    NgIf
+  ],
+  templateUrl: './users.component.html',
+  styleUrl: './users.component.css'
+})
+export class UsersComponent implements OnInit{
+  users: IUser[]
+  userDetails: IUser
+
+  constructor(private userService :UserService) {
+  }
+
+  ngOnInit(): void {
+    this.userService.getAll().subscribe(value => this.users = value)
+  }
+
+  getUser(userDetails: IUser) {
+    this.userDetails = userDetails
+  }
+}
